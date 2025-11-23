1110b4014e2849fc9203d71b50293375009a6f76
jlswbs <jlswbs@users.noreply.github.com>
2025-11-23 10:46:27 +0100

    Add files via upload

--- Vult_FmWrap/Vult_FmWrap.ino
+++ Vult_FmWrap/Vult_FmWrap.ino
@@ -1,4 +1,4 @@
-// Vult DSP float - FM sine wrap oscillator + reverb //
+// Vult DSP float - FM sine wrap oscillator //
 
 #include "CurieTimerOne.h"
 #include "Fmwrap.h"
@@ -8,15 +8,6 @@ Fmwrap_process_type fmwrap;
 #define AUDIO       5
 #define SAMPLE_RATE 44100
 #define BPM         120
-
-const int FIXED_BITS = 14;
-const int FIXED_SCALE = (1 << FIXED_BITS);
-const int REVERB_SIZE = 0x500;
-const int REVERB_LENGTH = (int)(REVERB_SIZE * 0.9f);
-const int REVERB_DECAY = (int)(FIXED_SCALE * 0.7f);
-
-int16_t reverbAddr;
-int reverbBuffer[REVERB_SIZE];
 
 float randomf(float minf, float maxf) {return minf + random(1UL << 31)*(maxf - minf) / (1UL << 31);} 
 
@@ -29,14 +20,7 @@ float randomf(float minf, float maxf) {return minf + random(1UL << 31)*(maxf - m
 
 void sample(){
 
-  int16_t sample = 24576.0f * Fmwrap_process(fmwrap, cv, rst, wrap);
-
-  int reverb = ((int)reverbBuffer[reverbAddr] * REVERB_DECAY) >> FIXED_BITS;
-  reverb += sample;
-  reverbBuffer[reverbAddr] = reverb;
-  reverbAddr++;
-  if (reverbAddr > REVERB_LENGTH) reverbAddr = 0;
-  int16_t output = sample + (reverbBuffer[reverbAddr]>>3);
+  int16_t output = 24576.0f * Fmwrap_process(fmwrap, cv, rst, wrap);
 
   analogWrite(AUDIO, 32768 + output);
     
